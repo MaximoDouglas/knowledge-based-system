edf2448694a4208cba491324458f022a112fee75
MaximoDouglas <dhms@ic.ufal.br>
2018-07-13 23:02:18 -0300

    City representation: Ok

---
+++ code/skynet 2.0/minicity/src/main/java/br/ufal/ic/ia2/minicity/model/Corner.java
@@ -0,0 +1,15 @@
+package br.ufal.ic.ia2.minicity.model;
+
+public class Corner {
+
+	private int posX;
+	private int posY;
+	private int id;
+	
+	public Corner(int posX, int posY, int id) {
+		this.posX = posX;
+		this.posY = posY;
+		this.id = id;
+	}
+	
+}
---
+++ code/skynet 2.0/minicity/src/main/java/br/ufal/ic/ia2/minicity/model/Semaphore.java
@@ -0,0 +1,14 @@
+package br.ufal.ic.ia2.minicity.model;
+
+import javafx.util.Pair;
+
+public class Semaphore {
+
+	private int redTime = 2;
+	private int greenTime = 2;
+	
+	private Pair<Integer, Integer> pos;
+	
+	
+	
+}
